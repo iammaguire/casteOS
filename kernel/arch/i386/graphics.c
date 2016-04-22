581c77c99db92fe9a73afd5c1557f7b77a6a2cf0
CMatri <connorperkins@cox.net>
2016-04-21 23:15:05 -0400

    text drawing, efficient area fill, better init

--- kernel/arch/i386/graphics.c
+++ kernel/arch/i386/graphics.c
@@ -1,18 +1,5 @@
 #include <kernel/multiboot.h>
-#include <stdint.h>
-#include <stddef.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-uint8_t *lfb;
-uint8_t pixel_width = 4; // booting into 32bbp so whatever
-
-uint16_t screen_width;
-uint16_t screen_height;
-uint16_t bytes_per_line;
-
-struct vbe_mode_info_block *info;
+#include <kernel/graphics.h>
 
 void graphics_install(struct multiboot* mbd)
 {
@@ -21,6 +8,15 @@ void graphics_install(struct multiboot* mbd)
 	bytes_per_line = info->pitch;
 	screen_width = info->Xres;
 	screen_height = info->Yres;
+
+	fill_rect(0, 0, screen_width, 10, 0xFFFFFF);
+	fill_rect(0, screen_height - 10, screen_width, 10, 0xFFFFFF);
+	fill_rect(0, 0, 10, screen_height, 0xFFFFFF);
+	fill_rect(screen_width - 10, 0, 10, screen_height, 0xFFFFFF);
+
+	fill_rect(15, 15, screen_width - 30, screen_height - 30, 0x20BB20);
+
+	draw_string("casteOS - by Connor Perkins", 11, 1, 0x000000, 0xFFFFFF);
 }
 
 void put_pixel(uint16_t x, uint16_t y, uint32_t color)
@@ -28,7 +24,46 @@ void put_pixel(uint16_t x, uint16_t y, uint32_t color)
 	if(x > screen_width || y > screen_height) return;
 
 	unsigned where = x * pixel_width + y * bytes_per_line;
-    	lfb[where] = color & 255;              	// BLUE
-    	lfb[where + 1] = (color >> 8) & 255;   	// GREEN
-    	lfb[where + 2] = (color >> 16) & 255;  	// RED
+    	lfb[where] = color & 255;
+    	lfb[where + 1] = (color >> 8) & 255;
+    	lfb[where + 2] = (color >> 16) & 255;
+}
+
+void fill_rect(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color)
+{
+	uint8_t *where = lfb + x * pixel_width + y * bytes_per_line; 
+
+	for(int i = 0; i < height; i++)
+	{
+		for(int j = 0; j < width; j++)
+		{
+			where[j * 4] = color & 255;
+			where[j * 4 + 1] = (color >> 8) & 255;
+			where[j * 4 + 2] = (color >> 16) & 255;
+		}
+
+		where += bytes_per_line;
+	}
+}
+
+void draw_char(uint8_t c, uint16_t x, uint16_t y, uint32_t foreground, uint32_t background)
+{
+	uint16_t cx, cy;
+	uint16_t mask[8] = { 1, 2, 4, 8, 16, 32, 64, 128 };
+
+	for(cy = 0; cy < 8; cy++)
+	{
+		for(cx = 0; cx < 8; cx++)
+		{
+			put_pixel(x + cx, y + cy, font8x8_basic[c][cy] & mask[cx] ? foreground : background);
+		}
+	}
+}
+
+void draw_string(char* string, uint16_t x, uint16_t y, uint32_t foreground, uint32_t background)
+{
+	for(int i = 0; i < strlen(string); i++)
+	{
+		draw_char(string[i], x + (i * 8), y, foreground, background);
+	}
 }
